b3283a00e75a79b8ea23a23f08d4d2cd1f87d01c
liangjun <530080147@qq.com>
2019-04-24 15:08:45 +0800

    修改文件位置，修改html模板语法

--- src/views/app/app.ts
+++ src/views/app/app.ts
@@ -1,5 +1,8 @@
-import config from "@/modules/js/config"
+import config from "@/config"
 import * as _ from 'lodash'
+import * as axios from 'axios'
 console.log(config.host)
 console.log(_.add)
+console.log(axios)
 const host = config.host
+
