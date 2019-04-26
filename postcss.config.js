6a2a01ee80bca830ab582503402468bcb3690d00
liangjun <530080147@qq.com>
2019-04-26 11:52:48 +0800

    禁止自动移除-webkit-等样式前缀

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,10 @@
 //postcss.config.js
 module.exports = {
     plugins: [
-        require('autoprefixer'),
+        require('autoprefixer')({
+            // 将不会自动移除自己添加的-webkit-样式
+            remove: false
+        }),
         require('cssnano')
     ]
 }
